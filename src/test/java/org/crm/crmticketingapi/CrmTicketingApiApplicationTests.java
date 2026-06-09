a07748651f7a34952430e9ba7a15df191fabbb35
Chaitanya Vinjamuri <chaitanyavinjamuri8@gmail.com>
2026-06-09 14:23:58 +0530

    Phase 1 and 2: Setup Spring Boot project and configure Hibernate SessionFactory with MySQL

---
+++ src/test/java/org/crm/crmticketingapi/CrmTicketingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.crm.crmticketingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmTicketingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
